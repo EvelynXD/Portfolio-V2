fe347a3c2f585d939d98f9d04d3885209b93aa95
EvelynXD <a8900873@gmail.com>
2023-10-14 12:46:23 +0700

    Portfolio V2

---
+++ src/components/portfolio.vue
@@ -0,0 +1,93 @@
+<template>
+    <div class="portfolio section">
+        <div class="text">
+            <h1 class="title">
+                <div class="outline-text">Portfolio</div>
+                <span class="hov">M</span>
+                <span class="hov">y</span>
+                <span class="space"></span>
+                <span class="hov">p</span>
+                <span class="hov">o</span>
+                <span class="hov">r</span>
+                <span class="hov">t</span>
+                <span class="hov">f</span>
+                <span class="hov">o</span>
+                <span class="hov">l</span>
+                <span class="hov">i</span>
+                <span class="hov">o</span>
+            </h1>
+            <p class="desc">A small gallery of my recent projects</p>
+        </div>
+        <div @click="menuBackgroundAnim(1)" ref="btn" class="btn">
+            <router-link to="/work">
+                See more!
+            </router-link>
+        </div>
+        <div class="gallery">
+            <div>
+                <a href="https://kuru-kuru.vercel.app/" target="_blank">
+                    <img src="../assets/herta-kuru_box.png" alt="" />
+                </a>
+            </div>
+            <div>
+                <a
+                    href="https://kebelet-iphone.vercel.app/"
+                    target="_blank"
+                >
+                    <img src="../assets/kebelet-iphone_box.png" alt="" />
+                </a>
+            </div>
+            <div>
+                <a
+                    href="https://evelynxd.github.io/firework-2023/"
+                    target="_blank"
+                >
+                    <img src="../assets/fireworks-2023_box.png" alt="" />
+                </a>
+            </div>
+            <div>
+                <a
+                    href="https://evelyn-terminal.vercel.app/"
+                    target="_blank"
+                >
+                    <img src="../assets/terminal-portfolio_box.png" alt="" />
+                </a>
+            </div>
+            <div>
+                <a
+                    href="https://evelynxd.vercel.app/"
+                    target="_blank"
+                >
+                    <img src="../assets/firts-portfolio_box.png" alt="" />
+                </a>
+            </div>
+            <div>
+                <a href="https://evelynxd.github.io/Snake-/" target="_blank">
+                    <img src="../assets/snake_box.png" alt="" />
+                </a>
+            </div>
+            <div>
+                <a href="https://statued.vercel.app/" target="_blank">
+                    <img src="../assets/statued_box.png" alt="" />
+                </a>
+            </div>
+            <div>
+                <a
+                    href="https://github.com/GunawanAhmad/Sisfo-Klinik-Full-Stack"
+                    target="_blank"
+                >
+                    <img src="../assets/kliniku_box.png" alt="" />
+                </a>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+import TransitionMixin from "../mixins/transition";
+
+export default {
+    mixins: [TransitionMixin],
+};
+</script>
+
+<style scoped src="../styles/work.css"></style>
---
+++ src/views/work.vue
@@ -0,0 +1,182 @@
+<template>
+    <div class="work view-page">
+        <div class="text">
+            <h1 class="title">
+                <div class="outline-text">Projects</div>
+                <span class="hov">M</span>
+                <span class="hov">y</span>
+                <span class="space"></span>
+                <span class="hov">p</span>
+                <span class="hov">r</span>
+                <span class="hov">o</span>
+                <span class="hov">j</span>
+                <span class="hov">e</span>
+                <span class="hov">c</span>
+                <span class="hov">t</span>
+                <span class="hov">s</span>
+            </h1>
+        </div>
+
+        <div class="work-list">
+            <div class="work" v-for="(work, index) in myProjects" :key="index">
+                <div class="information">
+                    <h2 class="work-number">0{{ index + 1 }}.</h2>
+                    <h1 class="work-title">
+                        {{ work.title }}
+                    </h1>
+                    <img
+                        :src="require(`@/assets/${work.img}`)"
+                        alt=""
+                        class="mobile-img"
+                    />
+                    <p class="description">
+                        {{ work.description }}
+                    </p>
+                    <div class="tools">
+                        <p>
+                            Code Tools:
+                            <span>{{ work.codeTools.join(", ") }}</span>
+                        </p>
+                        <p class="design">
+                            Design: <span>{{ work.design }}</span>
+                        </p>
+                    </div>
+                    <div class="links">
+                        <a
+                            target="_blank"
+                            class="btn"
+                            :href="work.link"
+                            :class="work.link === '' ? 'disabled' : ''"
+                            >Visit</a
+                        >
+                        <a
+                            target="_blank"
+                            class="btn"
+                            :href="work.source"
+                            :class="work.source === '' ? 'disabled' : ''"
+                            >Source Code</a
+                        >
+                    </div>
+                </div>
+                <div class="img">
+                    <img :src="require(`@/assets/${work.img}`)" alt="" />
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            isExpand: false,
+            icon: "fas fa-expand",
+            isDetail: false,
+            myProjects: [
+                {
+                    title: "Fireworks 2023",
+                    description:
+                        "Happy New Year!",
+                    source:
+                        "https://github.com/EvelynXD/firework-2023",
+                    link: "https://evelynxd.github.io/firework-2023/",
+                    img: "fireworks-2023.png",
+                    codeTools: ["HTML", "CSS", "Javascript"],
+                    design: "Design by Evelyn",
+                },
+                {
+                    title: "Kebelet Iphone",
+                    description:
+                        "This Kebelet Iphone is for people who have high prestige, but if you want to try it, you can.",
+                    source:
+                        "https://github.com/EvelynXD/Kebelet-Iphone",
+                    link:
+                        "https://kebelet-iphone.vercel.app/",
+                    img: "kebelet-iphone.png",
+                    codeTools: ["HTML", "CSS", "PHP"],
+                    design: "Design by Evelyn",
+                },
+                {
+                    title: "Herta Kuru",
+                    description:
+                        "The website for Herta, the cutest genius Honkai: Star Rail character out there.",
+                    source: "https://github.com/EvelynXD/kuru-kuru",
+                    link: "https://kuru-kuru.vercel.app/",
+                    img: "herta-kuru.png",
+                    codeTools: ["HTML", "CSS", "Javascript"],
+                    design: "Design by Haviza Aufa",
+                },
+                {
+                    title: "First Profile",
+                    description:
+                        "My first portfolio",
+                    source:
+                        "https://github.com/EvelynXD/first-profile",
+                    link:
+                        "https://evelynxd.vercel.app/",
+                    img: "firts-portfolio.png",
+                    codeTools: ["React JS"],
+                    design: "Design by Me",
+                },
+                {
+                    title: "Terminal Portfolio",
+                    description:
+                        "Terminal Portfolio is a portfolio website that is made to look like a terminal.",
+                    source:
+                        "https://github.com/EvelynXD/terminal",
+                    link: "https://evelyn-terminal.vercel.app/",
+                    img: "terminal-portfolio.png",
+                    codeTools: [
+                        "TypeScript",
+                        "React JS",
+                        "Next JS",
+                        "Tailwind CSS",
+                    ],
+                    design: "Design by Evelyn",
+                },
+                {
+                    title: "Game Snake",
+                    description:
+                        "This project can be accessed through a web browser and has been optimized for a responsive display, which means the game can be played on various devices, both desktop and mobile devices.",
+                    source: "https://github.com/EvelynXD/Snake-",
+                    link: "https://evelynxd.github.io/Snake-/",
+                    img: "snake.png",
+                    codeTools: ["HTML", "CSS", "Javascript"],
+                    design: "Design by Evelyn",
+                },
+                {
+                    title: "Statued (Landing Page)",
+                    description:
+                        "Online shop for statue customization with 3d look.",
+                    source: "https://github.com/GunawanAhmad/Statued",
+                    link: "https://statued.vercel.app/",
+                    img: "statued.png",
+                    codeTools: [
+                        "HTML",
+                        "Tailwind CSS",
+                        "Javascript",
+                        "Three Js",
+                    ],
+                    design: "Design by Igun",
+                },
+                {
+                    title: "Web Application for Online Consultation (Kliniku)",
+                    description:
+                        "Kliniku is an online consultation platform where people can fill in an online form about their health issue to send for a doctor. The doctor can respond and give them medical advice.",
+                    source:
+                        "https://github.com/EvelynXD/Sisfo-Klinik-Full-Stack",
+                    link: "",
+                    codeTools: ["VueJS", "MongoDB", "NodeJs", "ExpressJS"],
+                    design: "Design by Faris Azizy",
+                    img: "kliniku.png",
+                },
+            ],
+        };
+    },
+    mounted() {},
+    methods: {},
+};
+</script>
+
+<style scoped src="../styles/work.css"></style>
